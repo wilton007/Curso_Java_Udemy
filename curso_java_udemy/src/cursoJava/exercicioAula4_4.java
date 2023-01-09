3e5cfa6418c9a510bf41ffafa99822a26ba82e15
Wilton Jr <wilton007@hotmail.com>
2023-01-09 11:49:21 -0300

    Exercicio Aula 4.4

---
+++ curso_java_udemy/src/cursoJava/exercicioAula4_4.java
@@ -0,0 +1,16 @@
+package cursoJava;
+
+import java.util.Scanner;
+
+public class exercicioAula4_4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numeroFuncionario = input.nextInt();
+        int horasTrabalhadas = input.nextInt();
+        double salarioHoras = input.nextDouble();
+        double salarioTotal = (double) horasTrabalhadas * salarioHoras;
+
+        System.out.printf("NUMBER = %d\nSALARY = U$ %.2f", numeroFuncionario, salarioTotal);
+    }
+}
