fd0e30cbbe9315555df717a115bc48dc4fb51883
Wilton Jr <wilton007@hotmail.com>
2023-01-09 11:32:33 -0300

    Exercicio Aula 4.3

---
+++ curso_java_udemy/src/cursoJava/exercicioAula4_3.java
@@ -0,0 +1,18 @@
+package cursoJava;
+
+import java.util.Scanner;
+
+public class exercicioAula4_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int d = input.nextInt();
+
+        int total = a * b - c * d;
+
+        System.out.println("DIFERENCA = "+total);
+    }
+}
