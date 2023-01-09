823e99bfd8e52c68ca1422d685d2a40618944235
Wilton Jr <wilton007@hotmail.com>
2023-01-09 11:28:00 -0300

    Exercicio Aula 4.2

---
+++ curso_java_udemy/src/cursoJava/exercicioAula4_2.java
@@ -0,0 +1,14 @@
+package cursoJava;
+
+import java.util.Scanner;
+
+public class exercicioAula4_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double raiz = 3.14159;
+        double raio = input.nextDouble();
+        double area = raiz * (raio*raio);
+        System.out.printf("A = %.4f", area);
+    }
+}
