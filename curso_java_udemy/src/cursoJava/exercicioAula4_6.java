18304fb7d6dadda4830b2809222838a6dd8d8738
Wilton Jr <wilton007@hotmail.com>
2023-01-09 19:10:09 -0300

    Exercicio Aula 4.6

---
+++ curso_java_udemy/src/cursoJava/exercicioAula4_6.java
@@ -0,0 +1,28 @@
+package cursoJava;
+
+import java.util.Scanner;
+
+public class exercicioAula4_6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+
+        double triangulo = (a*c)/2.0;
+        double circulo =  3.14159 * (c*c);
+        double trapezio = (a+b)*c/2;
+        double quadrado = b*b;
+        double retangulo = a*b;
+
+        System.out.printf("TRIANGULO: %.3f\n",triangulo);
+        System.out.printf("CIRCULO: %.3f\n",circulo);
+        System.out.printf("TRAPEZIO: %.3f\n",trapezio);
+        System.out.printf("QUADRADO: %.3f\n",quadrado);
+        System.out.printf("RETANGULO: %.3f\n",retangulo);
+
+
+
+    }
+}
