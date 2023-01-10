503e75ccad2b4260a622caed8f0dcf8deef87f4e
Wilton Jr <wilton007@hotmail.com>
2023-01-10 10:48:11 -0300

    Exercicio Aula 5.1

---
+++ curso_java_udemy/src/cursoJava/exercicoAula5_1.java
@@ -0,0 +1,16 @@
+package cursoJava;
+
+import java.util.Scanner;
+
+public class exercicoAula5_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+        if (x < 0){
+            System.out.println("NEGATIVO");
+        }else {
+            System.out.println("NÃO NEGATIVO");
+        }
+    }
+}
