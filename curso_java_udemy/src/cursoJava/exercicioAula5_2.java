fd6875a03912c4b7d0baba1b51d064a9be498e7b
Wilton Jr <wilton007@hotmail.com>
2023-01-11 08:36:26 -0300

    Exercicio Aula 5.2

--- curso_java_udemy/src/cursoJava/exercicioAula5_2.java
+++ curso_java_udemy/src/cursoJava/exercicioAula5_2.java
@@ -2,12 +2,12 @@ package cursoJava;
 
 import java.util.Scanner;
 
-public class exercicoAula5_2 {
+public class exercicioAula5_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         int x = input.nextInt();
-        if (x % 2 == 0){
+        if (x % 2 == 0) {
             System.out.println("PAR");
         } else {
             System.out.println("IMPAR");
