d57fb49933816b3da5c13f827b0d7971fbac8362
Wilton Jr <wilton007@hotmail.com>
2023-01-12 10:10:09 -0300

    Exercicio Aula 6.2

---
+++ curso_java_udemy/src/cursoJava/exercicioAula6_2.java
@@ -0,0 +1,32 @@
+package cursoJava;
+
+import java.util.Scanner;
+
+public class exercicioAula6_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+        int y = input.nextInt();
+
+        while (x != 0 && y != 0) {
+            if (x > 0 && y > 0) {
+                System.out.println("primeiro");
+
+
+            } else if (x < 0 && y > 0) {
+                System.out.println("segundo");
+
+            } else if (x < 0) {
+                System.out.println("terceiro");
+
+            } else {
+                System.out.println("quarto");
+
+            }
+            x = input.nextInt();
+            y = input.nextInt();
+        }
+        System.out.println(" ");
+    }
+}
