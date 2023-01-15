55a3dedfee2bc23972302d7f4e8234bcee04f1ac
Wilton Jr <wilton007@hotmail.com>
2023-01-15 15:29:59 -0300

    Exercicio Aula 6.6

--- curso_java_udemy/src/cursoJava/exercicioAula6_6.java
+++ curso_java_udemy/src/cursoJava/exercicioAula6_6.java
@@ -9,9 +9,6 @@ public class exercicioAula6_6 {
 
         int n = input.nextInt();
         double x1, x2, x3, soma;
-        double soma1 = 0.0;
-        double soma2 = 0.0;
-        double soma3 = 0.0;
 
 
         for (int i = 0; i < n; i++) {
@@ -20,14 +17,9 @@ public class exercicioAula6_6 {
             x3 = input.nextDouble();
 
             soma = ((x1 * 2) + (x2 * 3) + (x3 * 5)) / (2 + 3 + 5);
-            if (i == 0) {
-                soma1 = soma;
-            } else if (i == 1) {
-                soma2 = soma;
-            } else {
-                soma3 = soma;
-            }
+            System.out.printf("%.1f", soma);
         }
-        PrintStream printf = System.out.printf("%.1f\n%.1f\n%.1f", soma1, soma2, soma3);
     }
+
 }
+
