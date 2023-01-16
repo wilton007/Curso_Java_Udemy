9052b42ab0ab105fe99442f08673a7c6d758f39d
Wilton Jr <wilton007@hotmail.com>
2023-01-16 09:10:52 -0300

    Exercicio Aula 6.7

---
+++ curso_java_udemy/src/cursoJava/exercicioAuala6_7.java
@@ -0,0 +1,24 @@
+package cursoJava;
+
+import java.util.Scanner;
+
+public class exercicioAuala6_7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        double a, b, resultado;
+
+        for (int i = 0; i < n; i++) {
+            a = input.nextDouble();
+            b = input.nextDouble();
+            resultado = a / b;
+            if (b == 0.0) {
+                System.out.println("divisao impossivel");
+            } else {
+                System.out.println(resultado);
+            }
+
+        }
+    }
+}
