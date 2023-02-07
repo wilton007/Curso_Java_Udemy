ac439ac52084f8e3314abee7278870a7ea774ca9
Wilton Jr <wilton007@hotmail.com>
2023-02-07 09:31:04 -0300

    Exercicio Aula 8.5

--- curso_java_udemy/src/cursoJava/ExercicioAula8_5.java
+++ curso_java_udemy/src/cursoJava/ExercicioAula8_5.java
@@ -1,21 +1,52 @@
 package cursoJava;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class ExercicioAula8_5 {
     public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("0.00");
         Scanner input = new Scanner(System.in);
-        ExercicioAula8_5_1 conta = new ExercicioAula8_5_1();
+        ExercicioAula8_5_1 conta;
+        double deposito;
 
         System.out.print("Entre com numero da sua conta: ");
         int numeroconta = input.nextInt();
-        conta.setNumeroconta(numeroconta);
         input.nextLine();
-        System.out.print("Nome do titular: ");
+        System.out.print("nomde do titular: ");
         String nome = input.nextLine();
-        conta.nometitular = nome;
-        conta.operacao();
-        conta.operacao2();
-        conta.operacao3();
+        System.out.print("Deseja fazer um deposito inicial (s/n)? ");
+        char resposta = input.next().charAt(0);
+        if (resposta == 's') {
+            System.out.print("quanto deseja deposita: ");
+            deposito = input.nextDouble();
+            conta = new ExercicioAula8_5_1(numeroconta, nome, deposito);
+        } else {
+            conta = new ExercicioAula8_5_1(numeroconta, nome);
+        }
+        System.out.println(" ");
+        System.out.println("status da conta");
+        System.out.println(conta);
+
+        System.out.println(" ");
+
+        System.out.print("quanto deseja deposita: ");
+        deposito = input.nextDouble();
+        conta.deposito(deposito);
+
+        System.out.println(" ");
+        System.out.println("status da conta");
+        System.out.println(conta);
+
+        System.out.println(" ");
+
+        System.out.print("quanto quer saquar: ");
+        double saque = input.nextDouble();
+        conta.saqeu(saque);
+
+        System.out.println(" ");
+        System.out.println("status da conta");
+        System.out.println(conta);
+
     }
 }
--- curso_java_udemy/src/cursoJava/ExercicioAula8_5_1.java
+++ curso_java_udemy/src/cursoJava/ExercicioAula8_5_1.java
@@ -1,63 +1,38 @@
 package cursoJava;
 
-import java.text.DecimalFormat;
-import java.util.Scanner;
-
 public class ExercicioAula8_5_1 {
-    Scanner input = new Scanner(System.in);
-    DecimalFormat df = new DecimalFormat("0.00");
 
-    private int numeroconta;
-    public String nometitular;
+    private final int numeroConta;
+    private final String nomeTitular;
     public double allconta;
 
-
-    public void setNumeroconta(int numeroconta) {
-        this.numeroconta = numeroconta;
-    }
-
-    public int getNumeroconta() {
-        return numeroconta;
-    }
-
-    public double operacao() {
-        System.out.print("Deseja fazer um deposito agora (s/n)? ");
-        String sn = input.next();
-        double totalconta = 0.0;
-
-        if (sn.equals("s")) {
-            System.out.print("Quanto quer depositar: ");
-            double valordeposito = input.nextDouble();
-            totalconta = totalconta + valordeposito;
-            System.out.println(" ");
-            System.out.println("dados da conta\nconta " + numeroconta + ", nome do titular: " + nometitular + ", saldo: $ " + df.format(totalconta));
-
-        } else {
-            System.out.println(" ");
-            System.out.println("dados da conta\nconta " + numeroconta + ", nome do titular: " + nometitular + ", saldo: $ " + df.format(totalconta));
-        }
-        allconta = totalconta;
-        return totalconta;
+    public ExercicioAula8_5_1(int numeroconta, String nometitular) {
+        this.numeroConta = numeroconta;
+        this.nomeTitular = nometitular;
 
     }
 
-    public double operacao2() {
-        System.out.println(" ");
-        System.out.print("quanto quer depositar: ");
-        double valordeposito = input.nextDouble();
-        allconta = allconta + valordeposito;
-        System.out.println("dados da conta\nconta " + numeroconta + ", nome do titular: " + nometitular + ", saldo: $ " + df.format(allconta));
-        return allconta;
-    }
-
-    public double operacao3() {
-        System.out.println(" ");
-        System.out.print("quanto quer sacar: ");
-        double valorsaque = input.nextDouble();
-        allconta = allconta - valorsaque - 5.00;
-        System.out.println("dados da conta\nconta " + numeroconta + ", nome do titular: " + nometitular + ", saldo: $ " + df.format(allconta));
-        return allconta;
+    public ExercicioAula8_5_1(int numeroconta, String nometitular, double allconta) {
+        this.numeroConta = numeroconta;
+        this.nomeTitular = nometitular;
+        deposito(allconta);
     }
 
 
+    public void deposito(double deposito){
+        allconta += deposito;
+
+    }
+    public void saqeu(double saque){
+        allconta -= saque + 5.00;
+
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Numero da conta: " + numeroConta +"\n"+
+                "Nome do titular: " + nomeTitular + "\n" +
+                "Saldo: " + allconta;
+    }
 }
