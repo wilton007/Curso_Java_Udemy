5042a239b40ae16537f77930a0fec3a26ef89e65
Wilton Jr <wilton007@hotmail.com>
2023-02-27 23:43:24 -0300

    Exercicio Aula 4.5

--- curso_java_udemy/src/cursoJava/ExercicioAula4_5.java
+++ curso_java_udemy/src/cursoJava/ExercicioAula4_5.java
@@ -2,19 +2,21 @@ package cursoJava;
 
 import java.util.Scanner;
 
-public class exercicioAula4_5 {
+public class ExercicioAula4_5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int codigo1 = input.nextInt();
+        System.out.print("Digite quantidade coxinha: ");
         int quantidade1 = input.nextInt();
+        System.out.print("Digite valor coxinha: ");
         double valor1 = input.nextDouble();
 
-        int codigo2 = input.nextInt();
+        System.out.print("Digite quantidade suco: ");
         int quantidade2 = input.nextInt();
+        System.out.print("Digite valor suco: ");
         double valor2 = input.nextDouble();
 
-        double valortotal = (double) (quantidade1*valor1)+(quantidade2*valor2);
+        double valortotal = (quantidade1*valor1) +(quantidade2*valor2);
 
         System.out.printf("VALOR A PAGAR = R$ %.2f",valortotal);
 
